2038fb4ab06db3c9930ca500ec58b6e016dc1c32
Zreo-Fire <992106950@qq.com>
2021-06-03 18:13:30 +0800

    插入排序

---
+++ insert_sort.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+def insert_sort(arr):
+    n = len(arr)
+    for i in range(1,n):
+        key=arr[i]
+        j = i-1
+        while j>=0 and key <arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1] = key
+if __name__=='__main__':
+    arr = [12,343,545,3,34,5,67]
+    insert_sort(arr)
+    print(arr)
